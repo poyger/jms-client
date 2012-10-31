23485a448fe149427f43ba3e10290929a70040ae
Poyan <poyan@ubuntu.(none)>
2012-10-31 13:02:35 +0100

    first commit

---
+++ src/main/java/GenericConsumer.java
@@ -0,0 +1,45 @@
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class GenericConsumer {
+
+    // Name of the queue we will receive messages from
+    private static String subject = "GENERICQ";
+
+    public static void main(String[] args) throws JMSException, NamingException {
+        Context jndiContext = new InitialContext();
+
+        // Getting JMS connection from the server
+        ConnectionFactory connectionFactory = (ConnectionFactory)jndiContext.lookup("ConnectionFactory");
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+
+        // Creating session for seding messages
+        Session session = connection.createSession(false,
+                Session.AUTO_ACKNOWLEDGE);
+
+        // Getting the queue 'TESTQUEUE'
+        Destination destination = session.createQueue(subject);
+
+        // MessageConsumer is used for receiving (consuming) messages
+        MessageConsumer consumer = session.createConsumer(destination);
+
+        // Here we receive the message.
+        // By default this call is blocking, which means it will wait
+        // for a message to arrive on the queue.
+        Message message = consumer.receive();
+
+        // There are many types of Message and TextMessage
+        // is just one of them. Producer sent us a TextMessage
+        // so we must cast to it to get access to its .getText()
+        // method.
+        if (message instanceof TextMessage) {
+            TextMessage textMessage = (TextMessage) message;
+            System.out.println("Received message '"
+                    + textMessage.getText() + "'");
+        }
+        connection.close();
+    }
+}
---
+++ src/main/java/GenericProducer.java
@@ -0,0 +1,44 @@
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class GenericProducer {
+
+    // Name of the queue we will be sending messages to
+    private static String subject = "dynamicQueues/FOO.BAR";
+
+    public static void main(String[] args) throws JMSException, NamingException {
+        Context jndiContext = new InitialContext();
+        ConnectionFactory connectionFactory =
+                (ConnectionFactory)jndiContext.lookup("ConnectionFactory");
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+
+        // JMS messages are sent and received using a Session. We will
+        // create here a non-transactional session object. If you want
+        // to use transactions you should set the first parameter to 'true'
+        Session session = connection.createSession(false,
+                Session.AUTO_ACKNOWLEDGE);
+
+        // Destination represents here our queue 'TESTQUEUE' on the
+        // JMS server. You don't have to do anything special on the
+        // server to create it, it will be created automatically.
+        Destination destination = (Destination) jndiContext.lookup(subject);
+
+        // MessageProducer is used for sending messages (as opposed
+        // to MessageConsumer which is used for receiving them)
+        MessageProducer producer = session.createProducer(destination);
+
+        // We will send a small text message saying 'Hello' in Japanese
+        TextMessage message = session.createTextMessage("POYANNN");
+
+        // Here we are sending the message!
+        producer.send(message);
+        System.out.println("Sent message '" + message.getText() + "'");
+
+        connection.close();
+
+    }
+
+}
